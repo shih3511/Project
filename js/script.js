73f2098e3e4e32893c32c0161034f91fbc6cd347
shih3511 <sonw1018@gmail.com>
2025-11-27 21:29:21 +0800

    翻轉效果，照片補齊

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // 2. 當任一按鈕被點案的時候
     // forEach 是一個迴圈，意思是指：「對於清單裡的每一個 button，都做一次以下動作...」
     buttons.forEach(button => {
-        
+
         button.addEventListener("click", function () {
             // 'this' 代表「現在被點到的這顆按鈕」
             // getAttribute("data-target") 會去讀取 HTML 裡寫的 data-target屬性 
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
             // true 代表「東西跑進畫面裡了」
             // false 代表「東西還在畫面外」
             if (entry.isIntersecting) {
-                entry.target.classList.add('active'); 
+                entry.target.classList.add('active');
             }
         });
     }, {
@@ -51,6 +51,47 @@ document.addEventListener("DOMContentLoaded", function () {
     animals.forEach(animal => {
         observer.observe(animal);
     });
+
+
+    // ==========================================
+    // 功能三：卡片翻轉進場動畫
+    // ==========================================
+
+    // 1. 選取所有需要翻轉效果的卡片
+    const cards = document.querySelectorAll(".card");
+    cards.forEach((card , index) => {
+        let delayTime = (index % 3) * 0.1;
+
+        card.style.transitionDelay = `${delayTime}s`;
+    });
+    // 2. 設定觀察器 (Observer)
+    const observerCards = new IntersectionObserver((entries, observer) => {
+
+        entries.forEach(entry => {
+            // 如果卡片進入畫面 (isIntersecting 為 true)
+            if (entry.isIntersecting) {
+                // 幫這張卡片加上 'visible' class，觸發 CSS 的動畫
+                entry.target.classList.add("visible");
+            }else{
+                entry.target.classList.remove("visible");
+            }
+        });
+    }, {
+        // 3. 設定門檻 (Option)
+        threshold: 0.4 // 當物件出現 30% 時觸發
+    });
+    // 4. 開始監視
+    cards.forEach(card => {
+        observerCards.observe(card);
+    });
+
+    // ==========================================
+    // 功能四：圖文進場動畫
+    // ==========================================
+    const aboutPics = document.getElementById("aboutPic");
+    const aboutTxts = document.getElementById("aboutTxt");
+
+
 
 });
 
